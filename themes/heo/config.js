7431883759fd73b4f351a33f2a933f49491e2e74
Qinan887 <152642043+Qinan887@users.noreply.github.com>
2024-09-21 12:31:55 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -19,16 +19,16 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享耳机',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_2: '维修案例',
+  HEO_HERO_TITLE_3: 'Afix',
+  HEO_HERO_TITLE_4: '案例',
+  HEO_HERO_TITLE_5: '专业的事，交给专业的人',
+  HEO_HERO_TITLE_LINK: 'https://afix.cc',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: 'bose', url: '/tag/必看精选' },
+  HEO_HERO_CATEGORY_2: { title: '索尼', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_3: { title: '森海塞尔', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
